8d41f772bca27584ce0f3634d7c0630f56e8dea8
Alice <hifumialice@gmail.com>
2024-04-22 20:59:59 +0900

    사용자 입력 받아서 계산하기 - 초안

--- Calculator/src/main/kotlin/Main.kt
+++ Calculator/src/main/kotlin/Main.kt
@@ -22,7 +22,7 @@ fun main() {
 //    cal.changeMod("*")
 //    cal.input(10.0,20.0)
 //    cal.changeMod("/")
-//    cal.input(10,20)
+//    cal.input(10.0,20.0)
 //    cal.changeMod("%")
 //    cal.input(10.0,20.0)
 
@@ -33,16 +33,20 @@ fun main() {
     var operator : String = ""
     var numberDouble : Double = 0.0
     var numberInt : Int = 0
+    var resultInt : Int = 0
     var resultDouble : Double = 0.0
     var i : Int = 0 // 첫번째 숫자인지 판별
-    var flag : Boolean = false // 입력받은게 정수인지 판별
-    var resultFlag : Boolean = false // 한번이라도 실수형 입력 받으면 True
+    var doubleFlag : Boolean = false // 한번이라도 실수형 입력 받으면 True
+    var intFlag : Boolean = false
+
+
+
     println("------------------------------------")
     println("계산기 사용법")
     println("숫자 입력창에 정수 또는 실수를 입력해주세요 ")
-    println("숫자 입력창에 \"그만\"을 입력하면 계산기 작동을 멈춥니다.")
-    println("숫자 입력창에 \"초기화\"을 입력하면 계산값이 초기화 됩니다.")
     println("연산 입력창에는 +, -, *, /, % 연산만 입력해주세요")
+    println("숫자 또는 연산 입력창에 \"그만\"을 입력하면 계산기 작동을 멈춥니다.")
+    println("숫자 또는 연산 입력창에 \"초기화\"을 입력하면 계산값이 초기화 됩니다.")
     println("------------------------------------")
 
     while(true){
@@ -59,86 +63,104 @@ fun main() {
             println("계산 결과를 초기화 합니다.")
             println("------------------------------------")
             i = -1
-            resultFlag = false
+            doubleFlag = false
             resultDouble = 0.0
+            resultInt = 0
         } else {
 
             try {
-                numberDouble = input.toDouble()
+                numberInt = input.toInt()
+                println("정수")
+                intFlag = true
+            } catch (e: NumberFormatException) {
 
-                if(numberDouble-numberDouble.toInt() == 0.0) { // 정수 입력
-                    numberInt = numberDouble.toInt()
-                    flag = true
-                } else { // 실수인경우
-                    resultFlag = true
+                println("정수 변환 불가")
+
+                try {  // 실수 변환
+                    numberDouble = input.toDouble()
+                    println("실수")
+                    doubleFlag = true
+                } catch (e: NumberFormatException ) {
+                    println("------------------------------------")
+                    println("정수 또는 실수를 입력해주세요")
+                    println("------------------------------------")
+                    continue
+                } catch(e : Exception) {
+                    println("------------------------------------")
+                    println("나도 모르는 에러 : ${e}")
+                    println("------------------------------------")
+                    continue
                 }
+            }
 
-                if (i == 0 ) {
-                    operator = returnOperator()
-                    if (operator == "초기화") {
-                        i = 0
-                        resultFlag = false
-                        resultDouble = 0.0
-                        continue
-                    } else if (operator == "그만") {
-                        break
-                    } else {
-                        cal.changeMod(operator)
-                    }
-                }
-
-                if (flag == true) { // 정수 입력일 떄
-                    if (i == 0) {
-                        resultDouble = numberInt.toDouble()
-                        flag = false
-                        i++
-                        continue
-                    }
-
-                    resultDouble = cal.input(resultDouble,numberInt)
-                    flag = false
-                } else { // 실수 입력일 때
-                    if (i == 0) {
-                        resultDouble = numberDouble
-                        i++
-                        continue
-                    }
-                    resultDouble = cal.input(resultDouble,numberDouble)
-                }
-                if (resultFlag == true) { // 실수를 보여줄 떄
-                    println("계산 결과 : ${resultDouble}")
-                } else {
-                    println("계산 결과 : ${resultDouble.toInt()}")
-                }
-
+            // 첫번쨰 수와 두번째 수를 입력 받는 중간에 한번만 실행됨
+            if (i == 0 ) {
+                println("i : ${i}")
                 operator = returnOperator()
                 if (operator == "초기화") {
                     i = 0
-                    resultFlag = false
+                    doubleFlag = false
                     resultDouble = 0.0
+                    resultInt = 0
                     continue
                 } else if (operator == "그만") {
                     break
                 } else {
+                    println("모드 체인지")
                     cal.changeMod(operator)
+
+                    if (intFlag == true) {
+                        resultInt = numberInt
+                        intFlag = false
+                    } else {
+                        resultDouble = numberDouble
+                    }
+                    i++
+                    continue
+
+                }
+            }
+            // 첫번쨰 수와 두번째 수를 입력 받는 중간에 한번만 실행됨
+
+            // 한번이라도 실수였던 적이 있으면
+            if (doubleFlag == true) {
+
+                if(intFlag == true) {
+                    resultDouble = cal.input(resultDouble,numberInt)
+                } else {
+                    resultDouble = cal.input(resultDouble,numberDouble)
                 }
 
+                println("계산 결과 : ${resultDouble}")
 
+            } else { // 실수가 된 적이 없다면
+                resultInt = cal.input(resultInt, numberInt)
+                intFlag = false
+                resultDouble = resultInt.toDouble()
+                println("계산 결과 : ${resultInt}")
 
-            } catch (e: NumberFormatException) { // 숫자로 바꾸는데 실패하면 처리
-                println("------------------------------------")
-                println("정수 또는 실수를 입력해주세요")
-                println("------------------------------------")
-            } catch (e : Exception) {
-                println("------------------------------------")
-                println("에러 발생 : " + e)
-                println("------------------------------------")
+            }
+
+            // 연산기호 정하기
+            operator = returnOperator()
+
+            if (operator == "초기화") {
+                i = 0
+                doubleFlag = false
+                resultDouble = 0.0
+                resultInt = 0
+                continue
+            } else if (operator == "그만") {
+                break
+            } else {
+                cal.changeMod(operator)
             }
 
         }
 
         i++ // 회수 올리기
     }
+
 
 }
 fun returnOperator() : String{
